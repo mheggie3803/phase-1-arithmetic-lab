691d7e2c01b38c952d1ff22ccb1b5dd0119d8922
Malcolm Heggie <mheggie@DESKTOP-8IG0D17.localdomain>
2021-08-29 20:02:19 -0400

    commit tree 7daa3ca2dc1a9671e4eff0116742982f35800c8c

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+
+//equation whose mutiplied results will eqaul 62
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+
+//generate a random integer greater than 0
+const random = Math.floor(Math.random() * 10) + 1;
+
+//mod equation that calculates a remainder that is equal to 4
+ let num3 = 19;
+ let num4 = 5;
+ let mod = num3 % num4;
+
+//max will return 20 as the highest number in the set
+let max = (1, 2, 3,15, 20)
+math.max(1, 2, 3, 15, 20);
